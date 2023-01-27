e7abb5078df9751ca444896001a11ef186c87450
fady <gamilfady605@gmail.com>
2023-01-27 09:01:56 +0200

    handle GET, POST, and POSTFORM requests using golang

--- 14webserver/go-client/main.go
+++ 14webserver/go-client/main.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 func main() {
 	fmt.Println("hello from go client")
 	GetRequest()
+	PostRequest()
+	PostFormRequest()
 }
 
 func GetRequest() {
@@ -31,7 +35,55 @@ func GetRequest() {
 	ContentByteData, err := ioutil.ReadAll(res.Body)
 	HandleNilErrors(err)
 	// convert it from byte data to normal readable string
-	fmt.Println("The Response Body -> ", string(ContentByteData))
+	fmt.Println("The Response of the GET request is -> ", string(ContentByteData))
+}
+
+func PostRequest() {
+	const url = "http://localhost:8000/post"
+
+	// TODO => fake json payload for my request body
+	reqBody := strings.NewReader(`
+      {
+         "username" : "magy",
+         "age" : 25
+      }
+   `)
+
+	// TODO => send the request
+	res, err := http.Post(url, "application/json", reqBody)
+	HandleNilErrors(err)
+
+	// TODO => close the request connection
+	defer res.Body.Close()
+
+	// TODO => read the response
+	ContentDataByte, err := ioutil.ReadAll(res.Body)
+	HandleNilErrors(err)
+	fmt.Println("The Response of the POST request is -> ", string(ContentDataByte))
+}
+
+func PostFormRequest() {
+	// HINT => this type of requests is receiving a url-encoded data
+
+	const server_url = "http://localhost:8000/postform"
+
+	// TODO => get the data ready
+	data := url.Values{}
+	data.Add("id", "1")
+	data.Add("firstname", "fady")
+	data.Add("lastname", "gamil")
+
+	// TODO => send the requst
+	res, err := http.PostForm(server_url, data)
+	HandleNilErrors(err)
+
+	// TODO => close the connection
+	defer res.Body.Close()
+
+	// TODO => handle the response
+	contentBytes, err := ioutil.ReadAll(res.Body)
+	HandleNilErrors(err)
+	fmt.Println("The Response of the POST request is -> ", string(contentBytes))
 }
 
 func HandleNilErrors(err error) {
